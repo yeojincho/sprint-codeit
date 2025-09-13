f4ae05f59e42dfa20583b6125421d77f80ede044
yeojin <yeojincho0129@gmail.com>
2025-09-13 19:47:40 +0900

    feat: 컴포넌트 UI 확인용 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import {Button} from "@/components/common/Button";
 import {Input} from "@/components/common/Input";
+import {Playground} from "@/components/common/Playground";
 import {TodoItem} from "@/components/todos/TodoItem";
 import {getTodos, createTodo, updateTodo, Todo} from "@/lib/api";
 import React, {useEffect, useState} from "react";
@@ -121,30 +122,7 @@ export default function HomePage() {
             </ul>
           </section>
         </div>
-
-        <div>
-          <h1 className="mb-4">화면 공통 컴포넌트</h1>
-          <div className="grid grid-cols-2 gap-4">
-            <Button type="add" iconClassName="">
-              추가하기
-            </Button>
-            <Button type="add" state="active">
-              추가하기
-            </Button>
-            <Button type="edit" iconClassName="">
-              수정완료
-            </Button>
-            <Button type="edit" state="active" iconClassName="">
-              수정하기
-            </Button>
-            <Button type="delete" state="active" iconClassName="">
-              삭제하기
-            </Button>
-            <Button type="add" state="" iconClassName=""></Button>
-            <Button type="add" state="active"></Button>
-            <TodoItem name="비타민 챙겨 먹기" />
-          </div>
-        </div>
+        {/* <Playground/> */}
       </div>
     </div>
   );
